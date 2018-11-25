97f3d6834c4bc8ba3e5f2d32560ba1cfce8baf38
David Reis <37869959+devilmakerx2@users.noreply.github.com>
2018-11-25 13:39:22 +0000

    -scafolding Pages/Manage

---
+++ ESW02-G02/ProjectSW/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjectSW.Areas.Identity.Data;
+
+namespace ProjectSW.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class ConfirmEmailModel : PageModel
+    {
+        private readonly UserManager<ProjectSWUser> _userManager;
+
+        public ConfirmEmailModel(UserManager<ProjectSWUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> OnGetAsync(string userId, string code)
+        {
+            if (userId == null || code == null)
+            {
+                return RedirectToPage("/Index");
+            }
+
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{userId}'.");
+            }
+
+            var result = await _userManager.ConfirmEmailAsync(user, code);
+            if (!result.Succeeded)
+            {
+                throw new InvalidOperationException($"Error confirming email for user with ID '{userId}':");
+            }
+
+            return Page();
+        }
+    }
+}

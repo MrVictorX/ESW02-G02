7adb33501561cf23d75a6652393b040413e87102
victor xavier <victortpxavier@gmail.com>
2018-12-01 03:44:55 +0000

    Tests 2

--- ESW02-G02/UnitTestProject1/UnitTest1.cs
+++ ESW02-G02/UnitTestProject1/UnitTest1.cs
@@ -1,21 +1,27 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-
+using System.Diagnostics;
 
 namespace UnitTestProject1
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            IWebDriver driver = new ChromeDriver();
+        IWebDriver driver;
 
-            // Open website
-            driver.Get("https://quintamiaoesw02-g02.azurewebsites.net/");
+        [SetUp]
+        public void StartBrowser()
+        {
+            driver = new ChromeDriver(@"C:\Users\victo\Documents\LEI\3ºAno\1ºSem\ESW\ESW02-G02\ESW02-G02\UnitTestProject1");
+        }
+
+        [Test]
+        public void Test()
+        {
+            driver.Navigate().GoToUrl("https://localhost:44384/Identity/Account/Register");
+
 
             // Find the link to registration form
             IWebElement link = driver.FindElement(By.Id("Registar"));
@@ -29,33 +35,38 @@ namespace UnitTestProject1
             IWebElement address = driver.FindElement(By.Id("Registar-address"));
             address.SendKeys("Rua");
             IWebElement date = driver.FindElement(By.Id("Registar-date"));
-            address.SendKeys("5/1/2008 8:30:52 AM");
+            date.SendKeys("05/01/1997");
             IWebElement type = driver.FindElement(By.Id("Registar-user-type"));
-            address.SendKeys("Funciario");
+            type.SendKeys("Funciario");
             IWebElement email = driver.FindElement(By.Id("Registar-email"));
-            address.SendKeys("manuel@hotmail.com");
+            email.SendKeys("manuel@hotmail.com");
             IWebElement password = driver.FindElement(By.Id("Registar-password"));
-            address.SendKeys("123");
+            password.SendKeys("123");
             IWebElement confirm = driver.FindElement(By.Id("Registar-confirm"));
-            address.SendKeys("123");
+            confirm.SendKeys("123");
 
             IWebElement submit = driver.FindElement(By.Id("Registar-submit"));
             submit.Submit();
-/*
-            // Check the sign up succeeded by checking that the randomized
-            // email appears in the website's header bar.
-            (new OpenQA.Selenium.Support.UI.WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>()
+            Stopwatch s = Stopwatch.StartNew();
+
+            string currentURL = driver.Url;
+            /*Assert.AreEqual(currentURL, "https://localhost:44384/");*/
+
+            if (currentURL.Equals("https://localhost:44384/"))
             {
-            public Boolean apply(WebDriver d)
-            {
-                WebElement header = d.findElement(By.id("header-login"));
-                return header.getText().contains(randomEmail);
+                s.Stop();
+                System.Console.WriteLine("Tempo decorrido: " + (s.ElapsedMilliseconds / 100));
+                NUnit.Framework.Assert.LessOrEqual(s.ElapsedMilliseconds, 30000);
             }
-        });
-        */
-        //Close the browser
-        driver.quit();
+            //Close the browser
+            driver.Quit();
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Close();
+        }
     }
 }
-    }
-}
+

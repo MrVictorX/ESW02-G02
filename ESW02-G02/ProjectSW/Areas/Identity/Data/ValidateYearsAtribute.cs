3314fbf9b382a46f8182f59bd02bd4f9d7ac14c1
David Reis <37869959+devilmakerx2@users.noreply.github.com>
2018-12-02 18:43:26 +0000

    -xml_tecnico

--- ESW02-G02/ProjectSW/Areas/Identity/Data/ValidateYearsAtribute.cs
+++ ESW02-G02/ProjectSW/Areas/Identity/Data/ValidateYearsAtribute.cs
@@ -6,17 +6,22 @@ using System.Threading.Tasks;
 
 namespace ProjectSW.Areas.Identity.Data
 {
+    /// <summary> Classe de validação, usada para validar a data inserida no campo "Data de nascimento"</summary>
     public class ValidateYearsAttribute : ValidationAttribute
     {
         private readonly DateTime _minValue = DateTime.UtcNow.AddYears(-99);
         private readonly DateTime _maxValue = DateTime.UtcNow.AddYears(-16);
 
+        /// <summary> Metodo de validação da data inserida, verifica se a data inserida representa uma data com no minimo de 16 anos</summary>
+        /// <param name="value">Objeto passado pelo input da Data de nascimento.</param>
         public override bool IsValid(object value)
         {
             DateTime val = (DateTime)value;
             return val >= _minValue && val <= _maxValue;
         }
 
+        /// <summary> Metodo que mostra uma mensagem de erro</summary>
+        /// <param name="name">Mensagem de erro passada caso necessário.</param>
         public override string FormatErrorMessage(string name)
         {
             return string.Format("O valor da sua data é invalida", _minValue, _maxValue);
--- ESW02-G02/ProjectSW/Controllers/UserController.cs
+++ ESW02-G02/ProjectSW/Controllers/UserController.cs
@@ -7,59 +7,21 @@ using ProjectSW.Models;
 
 namespace ProjectSW.Controllers
 {
+    /// <summary>Controlador Users, onde são executadas as ações relacionadas com os utilizadores</summary>
     public class UserController : Controller
     {
         private readonly ProjectSWContext _context;
 
+        /// <summary>Construtor que guarda o contexto da build</summary>
         public UserController(ProjectSWContext context)
         {
             _context = context;
         }
-        
-        public IActionResult Index()
-        {
-            return View();
-        }
-        public IActionResult ViewProfile()
-        {
-            return View();
-        }
 
-        public IActionResult EditProfile()
-        {
-            return View();
-        }
-
+        /// <summary>Ação que resulta na pagina com a lista de todos os utilizadores</summary>
         public IActionResult UserList()
         {
             return View(_context.Users.ToList());
         }
-
-        public IActionResult DeleteUser(string id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var user = _context.Users.FirstOrDefault(u => u.Id == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return View(user);
-        }
-
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string id)
-        {
-            var user = await _context.Users.FindAsync(id);
-            _context.Users.Remove(user);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(UserList));
-        }
-
     }
 }

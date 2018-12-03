fdb4d4bd3d3f6cf5c48dcd9b8be6989008c7f7b1
victor xavier <victortpxavier@gmail.com>
2018-12-03 17:27:37 +0000

    SendGrid 2

--- ESW02-G02/ProjectSW/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ESW02-G02/ProjectSW/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -111,7 +111,7 @@ namespace ProjectSW.Areas.Identity.Pages.Account
                     //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                     //  $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                    var apiKey = System.Environment.GetEnvironmentVariable("SENDGRID_APIKEY");
+                    var apiKey = System.Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
                     var client = new SendGridClient(apiKey);
                     var msg = new SendGridMessage()
                     {
